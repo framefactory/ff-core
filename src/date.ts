557d73ecfa9efa50871cdc1a6abc09503a5d0635
Ralph Wiedemeier <ralph@framefactory.ch>
2024-03-22 19:19:38 +0100

    add date functions

--- src/date.ts
+++ src/date.ts
@@ -10,3 +10,15 @@ export function toLocalISOString(date: Date)
     const tzOffset = date.getTimezoneOffset() * 60000; //offset in milliseconds
     return (new Date(date.getTime() - tzOffset)).toISOString().slice(0, -1);
 }
+
+export function isEqual(date1: Date, date2: Date)
+{
+    return date1.getTime() === date2.getTime();
+}
+
+export function isSameDate(date1: Date, date2: Date)
+{
+    return date1.getFullYear() === date2.getFullYear() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getDate() === date2.getDate();
+}

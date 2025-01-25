b5f94b1d07032c66e7a0ad52820740aa102aaba3
Ralph Wiedemeier <ralph@framefactory.ch>
2025-01-25 14:32:37 +0100

    add documentation

--- src/Lock.ts
+++ src/Lock.ts
@@ -5,28 +5,58 @@
  * License: MIT
  */
 
+/**
+ * Implements a simple lock that can be acquired and released.
+ * The `guard` method can be used to execute a function within the lock.
+ * The lock is released when the function returns.
+ */
 export class Lock
 {
     private promise: Promise<void>;
     private resolve: (() => void) | undefined;
 
+    /**
+     * Creates a new Lock instance.
+     */
     constructor()
     {
         this.promise = Promise.resolve();
         this.resolve = undefined;
     }
 
+    /**
+     * Acquires the lock. Returns a promise that resolves when the lock is acquired.
+     */
     async acquire(): Promise<void>
     {
         await this.promise;
         this.promise = new Promise<void>(resolve => (this.resolve = resolve));
     }
 
+    /**
+     * Releases the lock.
+     */
     release()
     {
         this.resolve?.();
     }
 
+    /**
+     * Executes the given function within the lock. The lock is released when the function returns.
+     * Example:
+     * ```
+     * const lock = new Lock();
+     * 
+     * async function myFunction(): Promise<void>
+     * {
+     *    await lock.guard(async () => {
+     *       // do something
+     *   });
+     * }
+     * ```
+     * @param fn The function to execute within the lock.
+     * @returns A promise with the result of the function.
+     */
     async guard<T>(fn: () => Promise<T>): Promise<T>
     {
         await this.acquire();
